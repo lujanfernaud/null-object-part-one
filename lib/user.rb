dc4e15c750e97b6426ece69ce49ac7d306463818
lujanfernaud <lujanfernaud@gmail.com>
2017-11-28 16:25:19 +0000

    Refactor User

--- lib/user.rb
+++ lib/user.rb
@@ -1,21 +1,15 @@
 class User
   include ActiveModel::Model
-  attr_accessor :credit_card, :subscription
+  attr_accessor :credit_card
+  attr_writer   :subscription
 
-  def initialize(arguments = {})
-    super
-    @subscription = arguments[:subscription] || NullSubscription.new
-  end
+  delegate :has_mentoring?, :price, to: :subscription
 
   def charge
     subscription.charge(credit_card)
   end
 
-  def has_mentoring?
-    subscription.has_mentoring?
-  end
-
-  def price
-    subscription.price
+  def subscription
+    @subscription ||= NullSubscription.new
   end
 end
